495547dc1153887c266505a130f6b8d486bf15ca
DevZi1la <developeranon85@gmail.com>
2024-04-27 17:51:05 +0200

    releases smart-order-router for taraxa testnet

---
+++ src/index.d.ts
@@ -0,0 +1,19 @@
+declare module '@uniswap/sdk-core' {
+  export * from '@0xelod/sdk-core';
+}
+
+declare module '@uniswap/v3-sdk' {
+  export * from '@0xelod/v3-sdk';
+}
+
+declare module '@uniswap/v2-sdk' {
+  export * from '@0xelod/v2-sdk';
+}
+
+declare module '@uniswap/router-sdk' {
+  export * from '@0xelod/router-sdk';
+}
+
+declare module '@uniswap/universal-router-sdk' {
+  export * from '@0xelod/universal-router-sdk';
+}
